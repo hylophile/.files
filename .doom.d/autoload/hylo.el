e779bdae688fa7d826473669c5ba56ed9c550b26
hylo <hylo@posteo.de>
2021-09-07 03:30:08 +0200

    config(doom): random theme update

--- .doom.d/autoload/hylo.el
+++ .doom.d/autoload/hylo.el
@@ -1,24 +1,43 @@
 ;;; autoload/hylo.el -*- lexical-binding: t; -*-
 
 ;;;###autoload
-(defun hylo/random-non-light-theme ()
-  (let ((theme (seq-random-elt (seq-remove
-                                (lambda (x)
-                                  (or (string-suffix-p "-light" (symbol-name x))
-                                      (string-prefix-p "light-" (symbol-name x))))
-                                (custom-available-themes)))))
+(defun hylo/random-dark-theme ()
+  (let ((theme (seq-random-elt (hylo/likeable-dark-themes))))
     (message (symbol-name theme))
     theme))
 
-;; (defun hylo/insert-theme () (insert (symbol-name doom-theme)))
+
+;;;###autoload
+(defun hylo/likeable-dark-themes ()
+  (seq-remove
+   (lambda (x)
+     (or
+      (eval
+       (cons 'or
+             (mapcar (lambda (y) (string=
+                                  (symbol-name x)
+                                  (symbol-name y)))
+                     '(
+                       ;; undesirable
+                       doom-plain-dark
+                       wombat
+                       ;; light
+                       light-blue
+                       doom-flatwhite
+                       whiteboard
+                       doom-plain))))
+      (string-suffix-p "-light" (symbol-name x))))
+   (custom-available-themes)))
 
 ;;;###autoload
 (defun hylo/insert-theme ()
   (insert
-   "\n"
    (+doom-dashboard--center
-    (- +doom-dashboard--width 0)
-    (with-temp-buffer
-      (insert (symbol-name doom-theme))
-      (buffer-string)))
-   "\n"))
+    +doom-dashboard--width
+    (concat
+     (propertize
+      "Theme loaded: "
+      'face 'doom-dashboard-menu-title)
+     (propertize (symbol-name doom-theme)
+                 'face '(:inherit (doom-dashboard-menu-desc bold)))))
+   "\n\n"))
--- .doom.d/config.el
+++ .doom.d/config.el
@@ -29,11 +29,15 @@
 ;; available. You can either set `doom-theme' or manually load a theme with the
 ;; `load-theme' function. This is the default:
 
-(setq doom-theme (hylo/random-non-light-theme))
+(setq doom-theme (hylo/random-dark-theme))
 (setq +doom-dashboard-functions (append
                                  (list (car +doom-dashboard-functions))
                                  '(hylo/insert-theme)
                                  (cdr +doom-dashboard-functions)))
+
+;; (after! magit
+;;   (setq magit-display-buffer-function #'magit-display-buffer-traditional))
+
 ;; If you use `org' and don't want your org files in the default location below,
 ;; change `org-directory'. It must be set before org loads!
 (setq org-directory "~/org/")
