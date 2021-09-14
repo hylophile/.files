a90f4ac020d151113b785904c90fb6a35eabda47
hylo <hylo@posteo.de>
2021-09-14 18:55:00 +0200

    config(symlinked): add config for slimblade

--- .config/.dotfiles/symlinked/setup.sh
+++ .config/.dotfiles/symlinked/setup.sh
@@ -1,2 +1,3 @@
 sudo ln -sfn ~/.config/.dotfiles/symlinked/logid.cfg /etc/logid.cfg
 sudo ln -sfn ~/.config/.dotfiles/symlinked/00-keyboard.conf /etc/X11/xorg.conf.d/00-keyboard.conf
+sudo ln -sfn ~/.config/.dotfiles/symlinked/99-slimblade.conf /etc/X11/xorg.conf.d/99-slimblade.conf
