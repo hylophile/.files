54aff0249323cb566577f9d63d5aa246d4832682
hylo <hylo@posteo.de>
2021-10-01 15:10:25 +0200

    config(mail): add mail stuff

--- .doom.d/load/mail.el
+++ .doom.d/load/mail.el
@@ -1,10 +1,19 @@
 ;;; mail.el -*- lexical-binding: t; -*-
 
 (set-email-account! "posteo"
-  '((mu4e-sent-folder       . "/mailbox/posteoSent")
-    (mu4e-drafts-folder     . "/mailbox/posteoDrafts")
-    (mu4e-trash-folder      . "/mailbox/posteoTrash")
+  '((mu4e-sent-folder       . "/posteo/Sent")
+    (mu4e-drafts-folder     . "/posteo/Drafts")
+    (mu4e-trash-folder      . "/posteo/Trash")
     ;; (mu4e-refile-folder     . "/posteo/All Mail")
     (smtpmail-smtp-user     . "mail")
+    (mu4e-compose-signature . "---\nname"))
+  t)
+
+(set-email-account! "uni"
+  '((mu4e-sent-folder       . "/uni/Sent")
+    (mu4e-drafts-folder     . "/uni/Entw&APw-rfe")
+    (mu4e-trash-folder      . "/uni/Trash")
+    ;; (mu4e-refile-folder     . "/uni/All Mail")
+    (smtpmail-smtp-user     . "unilmail")
     (mu4e-compose-signature . "---\nname"))
   t)
