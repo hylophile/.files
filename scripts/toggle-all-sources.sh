c46a8632443cdfa2e92cf4269f5626412312205d
hylo <hylo@posteo.de>
2022-05-25 01:29:24 +0200

    cfg: toggle all source also with keybind

---
+++ scripts/toggle-all-sources.sh
@@ -0,0 +1,15 @@
+#!/usr/bin/env bash
+default_source=$(pactl get-default-source)
+is_on=$(pactl list sources|rg -A 10 $default_source |rg "Mute: no")
+
+toggle_all()
+{
+    pactl list short sources | awk -v state=$1 '{system("pactl set-source-mute " $1 " " state)}'
+}
+
+if [ "$is_on" = "" ];
+then
+    toggle_all 'off'
+else
+    toggle_all 'on'
+fi
