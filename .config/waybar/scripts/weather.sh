cb375963265a1b13e24978dd6fda4a75049fb9de
hylo <hylo@posteo.de>
2022-07-24 15:29:35 +0200

    cfg(waybar): use brightsky(dwd) api for weather, not wttr.in

--- .config/waybar/scripts/weather.sh
+++ .config/waybar/scripts/weather.sh
@@ -1,4 +1,4 @@
-#!/bin/sh
+#!/usr/bin/env sh
 # weather information for location based on seen WiFi BSSIDs or IP as fallback
 
 # get close WiFi BSSIDs
@@ -17,13 +17,29 @@
 # fi
 
 # get weather information
-text="$(curl -s "https://wttr.in/berlin?format=%t")"
-tooltip="$(curl -s "https://wttr.in/berlin?0QT" |
-    sed 's/\\/\\\\/g' |
-    sed ':a;N;$!ba;s/\n/\\n/g' |
-    sed 's/"/\\"/g')"
+# text="$(curl -s "https://wttr.in/berlin?format=%t")"
+
+# tooltip="$(curl -s "https://wttr.in/berlin?0QT" |
+#     sed 's/\\/\\\\/g' |
+#     sed ':a;N;$!ba;s/\n/\\n/g' |
+#     sed 's/"/\\"/g')"
+
+
+temp=$(curl --silent "https://api.brightsky.dev/current_weather?lat=52.52&lon=13.29"|jq ".weather.temperature")
+
+if [ "$(echo "$temp" | head -c 1)" = "-" ]; then
+    text="$temp°C"
+else
+    text="+$temp°C"
+fi
+
+# curl "https://api.brightsky.dev/current_weather?lat=52.52&lon=13.29"|jq ".weather.temperature"
+
+# https://www.wetter.de/deutschland/wetter-berlin-18228265.html
+
 
 # output for Waybar
-if ! grep -q "Unknown location" <<< "$text"; then
-    echo "{\"text\": \"$text\", \"tooltip\": \"<tt>$tooltip</tt>\", \"class\": \"weather\"}"
-fi
+# if ! grep -q "Unknown location" <<< "$text"; then
+    # echo "{\"text\": \"$text\", \"tooltip\": \"<tt>$tooltip</tt>\", \"class\": \"weather\"}"
+echo "{\"text\": \"$text\", \"class\": \"weather\"}"
+# fi
