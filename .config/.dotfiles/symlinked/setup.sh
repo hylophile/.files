56b0778c6805f7bc451ecbe2f73b336fccc2c8fc
hylo <hylo@posteo.de>
2022-09-28 13:44:26 +0200

    cfg(firefox): add userChrome.css

--- .config/.dotfiles/symlinked/setup.sh
+++ .config/.dotfiles/symlinked/setup.sh
@@ -2,3 +2,4 @@ sudo ln -sfn ~/.config/.dotfiles/symlinked/logid.cfg /etc/logid.cfg
 sudo ln -sfn ~/.config/.dotfiles/symlinked/00-keyboard.conf /etc/X11/xorg.conf.d/00-keyboard.conf
 sudo ln -sfn ~/.config/.dotfiles/symlinked/99-slimblade.conf /etc/X11/xorg.conf.d/99-slimblade.conf
 sudo ln -sfn ~/.config/.dotfiles/symlinked/display-switch.ini /etc/conf.d/display-switch/display-switch.ini 
+# ln -sfn ~/.config/.dotfiles/symlinked/userChrome.css ~/.mozilla/firefox/yc9io9za.default-nightly/chrome/userChrome.css
