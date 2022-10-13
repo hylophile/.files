3f5dc84d0c82eb7e3d0fcb800d352598a0c99cb1
hylo <hylo@posteo.de>
2022-10-13 09:59:17 +0200

    cfg(polybar): urgency

---
+++ .config/polybar/urgency.sh
@@ -0,0 +1,16 @@
+#!/usr/bin/env sh
+names=$(swaymsg -t get_tree | jq -r "recurse(.nodes[]?) | select(.window_properties.instance==\"$1\").name")
+match=$(echo "$names" | grep -E -o '\([0-9]+\)')
+
+
+if [ "$2" = "show" ]; then
+    if [ "$match" = "" ]; then
+        exit 1
+    else
+        exit 0
+    fi
+fi
+
+if [ "$match" != "" ]; then
+    echo "%{T2}$2"
+fi
