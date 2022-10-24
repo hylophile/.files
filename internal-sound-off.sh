258b5be685c443a3fe6d69bd89ba39a18fed1025
hylo <hylo@posteo.de>
2022-10-24 09:19:10 +0200

    cfg: dock sound off

--- internal-sound-off.sh
+++ internal-sound-off.sh
@@ -2,3 +2,4 @@
 
 pactl set-card-profile alsa_card.pci-0000_07_00.1 off
 pactl set-card-profile alsa_card.pci-0000_07_00.6 off
+pactl set-card-profile alsa_card.usb-Generic_ThinkPad_Dock_USB_Audio-00 off
