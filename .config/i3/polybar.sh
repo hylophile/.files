b6dc68ff12b1eb3b567d6986058126893df3279a
hylo <hylo@posteo.de>
2022-11-11 12:36:20 +0100

    fix polybar startup

--- .config/i3/polybar.sh
+++ .config/i3/polybar.sh
@@ -2,7 +2,7 @@
 
 killall polybar
 
-for m in $(polybar --list-monitors | cut -d":" -f1); do
+for m in $(xrandr |rg "\sconnected" | cut -d' ' -f 1); do
     sleep 0.5
     MONITOR=$m polybar --reload top 2>/tmp/polybar_"$m" &
 done
