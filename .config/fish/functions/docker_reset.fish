2702cab7c8b9dce9d249e26a17cb9c283ec5b3ce
hylo <hylo@posteo.de>
2023-02-19 18:42:35 +0100

    fish: update docker_reset

--- .config/fish/functions/docker_reset.fish
+++ .config/fish/functions/docker_reset.fish
@@ -1,6 +1,11 @@
 #!/usr/bin/env fish
 
 function docker_reset
+    docker system prune
+    docker container stop (docker container ls -aq)
+    docker container rm (docker container ls -aq)
+    docker rmi (docker images -aq)
+    docker volume prune
     docker kill (docker ps -q)
     docker rm (docker ps -a -q)
     docker rmi (docker images -q)
