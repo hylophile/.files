77cbd395ee9d2c96322710f7083f71f7f7115959
hylo <hylo@posteo.de>
2023-02-19 18:42:07 +0100

    fish: add cargo/bin to path

--- .config/fish/config.fish
+++ .config/fish/config.fish
@@ -9,11 +9,11 @@ set -x FZF_CTRL_T_COMMAND 'fd --hidden .'
 # set -Ux BUN_INSTALL "/home/$USER/.bun"
 # set -px --path PATH "/home/$USER/.bun/bin"
 
-fish_add_path -p ~/.nix-profile/bin ~/.config/emacs/bin ~/.config/fisactl/bin ~/.local/bin
+fish_add_path -p ~/.nix-profile/bin ~/.config/emacs/bin ~/.config/fisactl/bin ~/.local/bin ~/.cargo/bin
 
 set -U NIX_PATH ~/.nix-defexpr/channels:/nix/var/nix/profiles/per-user/root/channels
 
-set -gx GUILE_LOAD_PATH ~/code/guile-script-utils ~/code/grump
+set -gx GUILE_LOAD_PATH ~/code/guile-script-utils ~/code/grump ~/code/guile-toml/ ~/code/guile-json
 
 set -U nvm_default_version v16.17.1
 
