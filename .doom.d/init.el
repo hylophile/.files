8de7c0a2c1f71f9e8c4b0562bd07524f2157fe1f
hylo <hylo@posteo.de>
2023-03-07 18:18:39 +0100

    doom: updates

--- .doom.d/init.el
+++ .doom.d/init.el
@@ -101,7 +101,8 @@
        (eval +overlay)     ; run code, run (also, repls)
        ;;gist              ; interacting with github gists
        lookup              ; navigate your code and its documentation
-       lsp               ; M-x vscode
+       ;; (lsp +eglot)               ; M-x vscode
+       lsp
        (magit +forge)             ; a git porcelain for Emacs
        ;; (magit )             ; a git porcelain for Emacs
        ;;make              ; run make tasks from Emacs
@@ -124,7 +125,7 @@
        ;;agda              ; types of types of types of types...
        ;;beancount         ; mind the GAAP
        (cc +lsp +tree-sitter)                ; C > C++ == 1
-       (clojure +lsp)           ; java with a lisp
+       ;;(clojure +lsp)           ; java with a lisp
        ;;common-lisp       ; if you've seen one lisp, you've seen them all
        ;;coq               ; proofs-as-programs
        ;;crystal           ; ruby at the speed of c
@@ -136,13 +137,13 @@
        ;;elm               ; care for a cup of TEA?
        emacs-lisp        ; drown in parentheses
        ;;erlang            ; an elegant language for a more civilized age
-       (ess +stan)               ; emacs speaks statistics
+       ;;(ess +stan)               ; emacs speaks statistics
        ;;factor
        ;;faust             ; dsp, but you get to keep your soul
        ;;fsharp            ; ML stands for Microsoft's Language
        ;;fstar             ; (dependent) types and (monadic) effects and Z3
        ;;gdscript          ; the language you waited for
-       (go +lsp +tree-sitter)         ; the hipster dialect
+       ;;(go +lsp +tree-sitter)         ; the hipster dialect
        ;;(haskell +dante)  ; a language that's lazier than I am
        ;;hy                ; readability of scheme w/ speed of python
        ;;idris             ; a language you can depend on
@@ -185,7 +186,7 @@
        ;;terra             ; Earth and Moon in alignment for performance.
        (web +html +css +lsp +tree-sitter)               ; the tubes
        (yaml +lsp)              ; JSON, but readable
-       (zig +lsp +tree-sitter)               ; C, but simpler
+       ;;(zig +lsp +tree-sitter)               ; C, but simpler
 
        :email
        (mu4e +org +gmail)
@@ -196,7 +197,7 @@
        ;;calendar
        ;;
        ;;emms
-       everywhere        ; *leave* Emacs!? You must be joking
+       ;;everywhere        ; *leave* Emacs!? You must be joking
        ;;irc               ; how neckbeards socialize
        ;;(rss +org)        ; emacs as an RSS reader
        ;;twitter           ; twitter client https://twitter.com/vnought
