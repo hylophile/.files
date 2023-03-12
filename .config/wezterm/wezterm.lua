180b03b91da6b72bc166b903e5e75cbb385680c2
hylo <hylo@posteo.de>
2023-03-12 12:50:43 +0100

    feat(wezterm): nicer tab title format

--- .config/wezterm/wezterm.lua
+++ .config/wezterm/wezterm.lua
@@ -45,7 +45,18 @@ wezterm.on("spawn-bachelor", function(window, pane)
 	-- })
 end)
 
+wezterm.on("format-tab-title", function(tab) --, tabs, panes, config, hover, max_width)
+	-- if tab.is_active then
+	--   return {
+	--     { Background = { Color = 'blue' } },
+	--     { Text = ' ' .. tab.active_pane.title .. ' ' },
+	--   }
+	-- end
+	return "   " .. tab.active_pane.title .. "   "
+end)
+
 -- wezterm.on("gui-startup", function(cmd)
+
 -- 	-- allow `wezterm start -- something` to affect what we spawn
 -- 	-- in our initial window
 -- 	local args = {}
@@ -264,7 +275,30 @@ return {
 	font = wezterm.font("agave"),
 	font_size = 15.0,
 	use_fancy_tab_bar = false,
+	-- tab_bar_at_bottom = true,
+	tab_max_width = 100,
 	enable_scroll_bar = true,
+	window_frame = {
+		-- The font used in the tab bar.
+		-- Roboto Bold is the default; this font is bundled
+		-- with wezterm.
+		-- Whatever font is selected here, it will have the
+		-- main font setting appended to it to pick up any
+		-- fallback fonts you may have used there.
+		font = wezterm.font("Jost*"),
+
+		-- The size of the font in the tab bar.
+		-- Default to 10. on Windows but 12.0 on other systems
+		font_size = 12.0,
+
+		-- The overall background color of the tab bar when
+		-- the window is focused
+		active_titlebar_bg = "#1e1f29",
+
+		-- The overall background color of the tab bar when
+		-- the window is not focused
+		inactive_titlebar_bg = "#1e1f29",
+	},
 	colors = {
 		cursor_bg = "#44475a",
 		-- -- Overrides the text color when the current cell is occupied by the cursor
