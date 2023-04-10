9db86e10346af871d0c53c6a62affe9b1ea0d14d
hylo <hylo@posteo.de>
2023-04-10 14:33:42 +0200

    fix slow wezterm startup

--- scripts/wezterm-start-on-current-workspace.fish
+++ scripts/wezterm-start-on-current-workspace.fish
@@ -1,8 +1,7 @@
 #!/usr/bin/env fish
 
-set ws (wezterm cli list-clients --format json | jq -r '.[].workspace')
-
-if test -n "$ws"
+if pgrep wezterm-gui
+    set ws (wezterm cli list-clients --format json | jq -r '.[].workspace')
     wezterm start --workspace $ws
 else
     wezterm start
