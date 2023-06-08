16d750a0bbee83eea448a0e816dbee282e62247f
hylo <hylo@posteo.de>
2023-06-08 13:40:04 +0200

    feat(fish): show python version for venv

--- .config/fish/functions/fish_prompt.fish
+++ .config/fish/functions/fish_prompt.fish
@@ -132,7 +132,8 @@ function fish_prompt
     set -q VIRTUAL_ENV_DISABLE_PROMPT
     or set -g VIRTUAL_ENV_DISABLE_PROMPT true
     set -q VIRTUAL_ENV
-    and _nim_prompt_wrapper $retc V (basename "$VIRTUAL_ENV")
+    # and _nim_prompt_wrapper $retc V (basename "$VIRTUAL_ENV")
+    and _nim_prompt_wrapper $retc V (python --version)
 
     # git
     set -l prompt_git (fish_git_prompt '%s')
