443f804c5c31fad442221c5ff21f090b3c6de316
hylo <hylo@posteo.de>
2023-07-05 19:29:22 +0200

    fix: don't wrap with base64

--- .config/fish/config.fish
+++ .config/fish/config.fish
@@ -20,8 +20,9 @@ set -gx VISUAL nvim
 bind \cH backward-kill-word
 bind \cw backward-kill-bigword
 
-function set_wezterm_prog --on-event fish_preexec
-    printf "\033]1337;SetUserVar=%s=%s\007" WEZTERM_PROG (echo -n $argv | base64)
+function set_wezterm_user_vars --on-event fish_preexec
+    printf "\033]1337;SetUserVar=%s=%s\007" WEZTERM_PROG (echo -n $argv | base64 --wrap 0)
+    printf "\033]1337;SetUserVar=%s=%s\007" WEZTERM_CWD (echo -n (pwd) | base64 --wrap 0)
 end
 
 function unset_wezterm_prog --on-event fish_postexec
