b661f3aa3b244972b67f58bc025d1e8d50d68a4e
hylo <hylo@posteo.de>
2023-07-22 13:31:54 +0200

    feat: darkman arc

--- .local/share/light-mode.d/gtk-theme.sh
+++ .local/share/light-mode.d/gtk-theme.sh
@@ -2,5 +2,5 @@
 
 # Note: The names for the Arc theme variations are terrible.
 # "Darker" is actually LESS DARK than "Dark".
-gsettings set org.gnome.desktop.interface gtk-theme 'Arc-Lighter'
+gsettings set org.gnome.desktop.interface gtk-theme 'Arc'
 gsettings set org.gnome.desktop.interface color-scheme 'prefer-light'
