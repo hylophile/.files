5d622053b3ff96305c74a7d277af9e5d8cfb73b0
hylo <hylo@posteo.de>
2023-09-12 13:15:35 +0200

    feat(sway): mark (1) windows urgent

---
+++ scripts/notify.clj
@@ -0,0 +1,21 @@
+#!/usr/bin/env bb
+(require '[babashka.process :as p :refer [shell process destroy-tree]]
+         '[clojure.java.io :as io])
+
+(def workspace-stream
+  (process
+   {:err :inherit
+    :shutdown destroy-tree}
+   (str "swaymsg -t subscribe -m \"['window']\"")))
+
+(defn process-window [window]
+  (let [container (:container window)]
+    (when (-> container :name (str/includes? "(1)"))
+      (shell (format "swaymsg [con_id=%s] urgent enable" (:id container))))))
+
+(with-open [rdr (io/reader (:out workspace-stream))]
+  (binding [*in* rdr]
+    (loop []
+      (when-let [line (-> (read-line) (json/parse-string true))]
+        (process-window line)
+        (recur)))))
