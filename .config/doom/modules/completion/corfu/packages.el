bd453e5dcb6d45e6f04f1b274b4c9ae3aa27647a
hylo <hylo@posteo.de>
2023-09-20 23:14:36 +0200

    feat(doom): update corfu

--- .config/doom/modules/completion/corfu/packages.el
+++ .config/doom/modules/completion/corfu/packages.el
@@ -7,12 +7,16 @@
   (package! kind-icon))
 (when (modulep! +orderless)
   (package! orderless))
-(package! corfu-doc
-  :recipe (:host github :repo "galeo/corfu-doc"))
 (package! cape)
-(package! popon
-  :recipe (:type git :repo "https://codeberg.org/akib/emacs-popon"))
-(package! corfu-terminal
-  :recipe (:type git :repo "https://codeberg.org/akib/emacs-corfu-terminal.git"))
-(package! corfu-doc-terminal
-  :recipe (:type git :repo "https://codeberg.org/akib/emacs-corfu-doc-terminal.git"))
+(package! cape-yasnippet
+  :recipe (:host github :repo "elken/cape-yasnippet"))
+(package! cape-use-package
+  :recipe (:host github :repo "elken/cape-use-package"))
+
+(when (modulep! :os tty)
+  (package! popon
+    :recipe (:type git :repo "https://codeberg.org/akib/emacs-popon"))
+  (package! corfu-terminal
+    :recipe (:type git :repo "https://codeberg.org/akib/emacs-corfu-terminal.git"))
+  (package! corfu-doc-terminal
+    :recipe (:type git :repo "https://codeberg.org/akib/emacs-corfu-doc-terminal.git")))
