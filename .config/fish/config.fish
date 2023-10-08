9cc2c02c7c2a8f46b771c5b02d61236441ca2966
hylo <hylo@posteo.de>
2023-10-08 19:13:38 +0200

    refactor: simplify fish config

--- .config/fish/config.fish
+++ .config/fish/config.fish
@@ -1,24 +1,24 @@
 #bind -k f3 'printf ""'
 
-set -x MOZ_ENABLE_WAYLAND 1
+# set -x MOZ_ENABLE_WAYLAND 1
 
-set -x FZF_ALT_C_COMMAND 'fd --type directory --hidden .'
-set -x FZF_CTRL_T_COMMAND 'fd --hidden .'
+# set -x FZF_ALT_C_COMMAND 'fd --type directory --hidden .'
+# set -x FZF_CTRL_T_COMMAND 'fd --hidden .'
 
 fish_add_path -p ~/.config/emacs/bin
-fish_add_path -p ~/.config/fisactl/bin
+# fish_add_path -p ~/.config/fisactl/bin
 
 # set -U NIX_PATH ~/.nix-defexpr/channels:/nix/var/nix/profiles/per-user/root/channels
 
-set -U nvm_default_version v16.19.1
+# set -U nvm_default_version v16.19.1
 
-set -gx EDITOR nvim
-set -gx VISUAL nvim
+# set -gx EDITOR nvim
+# set -gx VISUAL nvim
 
 #source ~/.config/fish/hm-session-vars.fish
 
-bind \cH backward-kill-word
-bind \cw backward-kill-bigword
+# bind \cH backward-kill-word
+# bind \cw backward-kill-bigword
 
 function set_wezterm_user_vars --on-event fish_preexec
     printf "\033]1337;SetUserVar=%s=%s\007" WEZTERM_PROG (echo -n $argv | base64 --wrap 0)
--- .config/fish/functions/fish_greeting.fish
+++ .config/fish/functions/fish_greeting.fish
@@ -1,3 +1,3 @@
 function fish_greeting
-    fortune all
+    true
 end
