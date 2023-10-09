477326d29069516f25d6b39dec099555a7a0b197
hylo <hylo@posteo.de>
2023-10-09 14:41:12 +0200

    feat: add elisp script to tangle org files from the command line

---
+++ scripts/tangle.el
@@ -0,0 +1,16 @@
+#!/usr/bin/emacs --script
+
+(require 'org)
+(require 'ox)
+(require 'ob-tangle)
+
+(setq org-confirm-babel-evaluate nil)
+
+(org-babel-do-load-languages 'org-babel-load-languages
+                             '((shell . t)))
+
+(let* ((file (pop argv)))
+  (with-current-buffer (find-file-noselect file)
+    (org-export-expand-include-keyword)
+    (ignore-errors (org-babel-execute-buffer))
+    (org-babel-tangle nil nil)))
