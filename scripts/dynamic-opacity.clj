6806e1d5ad7c4d610a07101ce2e1a595336b9f73
hylo <hylo@posteo.de>
2023-10-13 11:18:36 +0200

    fix(sway): we only care about dynamic opacity with firefox

--- scripts/dynamic-opacity.clj
+++ scripts/dynamic-opacity.clj
@@ -12,21 +12,15 @@
   (binding [*in* rdr]
     (loop []
       (when-let [line (-> (read-line) (json/parse-string true))]
-          ;; (println line)
-          ;; (println)
           (when (or (= (:change line) "focus")
                     (= (:change line) "title")
                     (= (:change line) "floating"))
               (let [container (:container line)
                     name (:name container)
                     app-id (:app_id container)]
-               (if (or (= (:type container) "floating_con")
-                       (= app-id "neovide")
-                       (= app-id "emacs")
-                       (= app-id "org.wezfurlong.wezterm")
-                       (and
-                           (= app-id "firefoxdeveloperedition")
-                           (str/includes? name "YouTube")))
-                 (shell "swaymsg opacity set 1.0")
-                 (shell "swaymsg opacity set 0.95"))))
+               (when (= app-id "firefox-aurora")
+                 (if (or (= (:type container) "floating_con")
+                         (str/includes? name "YouTube"))
+                   (shell "swaymsg opacity set 1.0")
+                   (shell "swaymsg opacity set 0.95")))))
         (recur)))))
