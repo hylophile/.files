3f98504ea78708d913b16cce86faf5299b671595
hylo <hylo@posteo.de>
2023-12-29 15:52:19 +0100

    feat: script for starting remarkable pen & touchpad input

---
+++ scripts/remarkable.fish
@@ -0,0 +1,48 @@
+#!/usr/bin/env fish
+set rem_bin ~/code/misc/zig-rem-input/zig-out/bin/zig-rem-input
+set rem_ip 0
+
+ping -c 1 -W 1 remarkable-usb >/dev/null 2>&1
+
+if test $status -eq 0
+    set rem_ip remarkable-usb
+else
+    ping -c 1 -W 1 remarkable-wifi >/dev/null 2>&1
+    if test $status -eq 0
+        set rem_ip remarkable-wifi
+    else
+        printf "can't find remarkable\n"
+        exit 1
+    end
+end
+
+printf "remarkable is at $rem_ip\n"
+
+printf "starting:\n"
+
+printf "- stopping xochitl\n"
+ssh root@$rem_ip 'systemctl stop xochitl'
+
+printf "- forwarding touchpad\n"
+ssh root@$rem_ip 'cat /dev/input/event2' | $rem_bin touchpad &
+
+printf "- forwarding pen\n"
+ssh root@$rem_ip 'cat /dev/input/event1' | $rem_bin pen &
+
+printf "started!\n"
+
+function cleanup
+    printf "\n"
+    printf "cleaning up:\n"
+
+    printf "- starting xochitl\n"
+    ssh root@$rem_ip 'systemctl start xochitl'
+
+    printf "- killing zig-rem-input\n"
+    pkill zig-rem-input
+end
+
+trap cleanup INT
+
+sleep infinity
+# tail -f /dev/null
