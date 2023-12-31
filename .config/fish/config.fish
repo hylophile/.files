08a5996ebd067fbeedfc1ad3d63d125f84c4b527
hylo <hylo@posteo.de>
2023-12-31 20:51:53 +0100

    feat: auto-switch into distroboxes

--- .config/fish/config.fish
+++ .config/fish/config.fish
@@ -1,8 +1,8 @@
-if command -v zoxide >/dev/null
+if command -q zoxide
     zoxide init fish --cmd d | source
 end
 
-if command -v direnv >/dev/null
+if command -q direnv
     direnv hook fish | source
 end
 
@@ -13,6 +13,35 @@ end
 
 function unset_wezterm_prog --on-event fish_postexec
     printf "\033]1337;SetUserVar=%s=%s\007" WEZTERM_PROG ""
+end
+
+if command -q distrobox; and command -q direnv
+    function distrobox-exit-auto --on-event fish_postexec
+        if test -f /run/.containerenv; and direnv status | rg "No .envrc or .env found" >/dev/null
+            set -U DISTROBOX_PRE_EXIT_PWD (pwd)
+            exit
+        end
+    end
+
+    function distrobox-enter-auto --on-event fish_prompt
+        if not set --query DISTROBOX_ENTER_NAME
+            return
+        end
+
+        if not test -f /run/.containerenv
+            if distrobox list | cut -d' ' -f3 | rg $DISTROBOX_ENTER_NAME >/dev/null
+                distrobox enter $DISTROBOX_ENTER_NAME #-- fish -C 'direnv reload'
+                # this runs after exiting the distrobox
+                if set --query DISTROBOX_PRE_EXIT_PWD
+                    cd $DISTROBOX_PRE_EXIT_PWD
+                    set -U --erase DISTROBOX_PRE_EXIT_PWD
+                end
+            else
+                printf "Tried entering container %s, but it doesn't exist.\n" $DISTROBOX_ENTER_NAME
+                set status 1
+            end
+        end
+    end
 end
 
 set -U XDG_CURRENT_DESKTOP sway
