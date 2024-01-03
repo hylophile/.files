51e7dd2b657f0008b3ca306fa478cc0ca1fe197e
hylo <hylo@posteo.de>
2024-01-03 22:18:02 +0100

    fix: use base64 -w (seems to be more agnostic)

--- .config/fish/config.fish
+++ .config/fish/config.fish
@@ -7,8 +7,8 @@ if command -q direnv
 end
 
 function set_wezterm_user_vars --on-event fish_preexec
-    printf "\033]1337;SetUserVar=%s=%s\007" WEZTERM_PROG (echo -n $argv | base64 --wrap 0)
-    printf "\033]1337;SetUserVar=%s=%s\007" WEZTERM_CWD (echo -n (pwd) | base64 --wrap 0)
+    printf "\033]1337;SetUserVar=%s=%s\007" WEZTERM_PROG (echo -n $argv | base64 -w 0)
+    printf "\033]1337;SetUserVar=%s=%s\007" WEZTERM_CWD (echo -n (pwd) | base64 -w 0)
 end
 
 function unset_wezterm_prog --on-event fish_postexec
