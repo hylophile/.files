ae1ec33dc99c0542ff41e7274044f324b1a0fe4e
hylo <hylo@posteo.de>
2024-01-08 11:20:42 +0100

    feat: boot into windows partition with systemd-boot

--- .config/fish/functions/windows.fish
+++ .config/fish/functions/windows.fish
@@ -1,5 +1,10 @@
 function windows
-set windows_title (sudo grep -i windows /boot/grub/grub.cfg|cut -d"'" -f2)
-sudo grub-reboot $windows_title
-systemctl reboot
+    # for grub
+    # set windows_title (sudo grep -i windows /boot/grub/grub.cfg|cut -d"'" -f2)
+    # sudo grub-reboot $windows_title
+    # systemctl reboot
+
+    # for systemd-boot
+    sudo bootctl set-oneshot auto-windows
+    systemctl reboot
 end
