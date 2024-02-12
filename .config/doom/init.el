bef295a06c8346f839334e7c2111fa73f66914a2
hylo <hylo@posteo.de>
2024-02-12 17:06:08 +0100

    feat(doom): latex

--- .config/doom/init.el
+++ .config/doom/init.el
@@ -153,10 +153,10 @@
        (javascript +lsp +tree-sitter)        ; all(hope(abandon(ye(who(enter(here))))))
                                         ;(julia +lsp)             ; a better, faster MATLAB
        ;;kotlin            ; a better, slicker Java(Script)
-       ;;(latex             ; writing papers in Emacs has never been so fun
-       ;; +latexmk
-       ;; +cdlatex
-       ;; +fold)
+       (latex             ; writing papers in Emacs has never been so fun
+        +latexmk
+        +cdlatex
+        +fold)
        ;;lean              ; for folks with too much to prove
        ;;ledger            ; be audit you can be
        (lua +lsp +tree-sitter)               ; one-based indices? one-based indices
