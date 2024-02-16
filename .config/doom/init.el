5fa1cece05a01005adada38ad48159f0924df4ba
hylo <hylo@posteo.de>
2024-02-16 21:52:21 +0100

    chore: cleanup doom config

--- .config/doom/init.el
+++ .config/doom/init.el
@@ -40,7 +40,7 @@
        (emoji +unicode)  ; 🙂
        hl-todo           ; highlight TODO/FIXME/NOTE/DEPRECATED/HACK/REVIEW
        ;;       hydra
-       ;; indent-guides     ; highlighted indent columns
+       ;; indent-guides     ;highlighted indent columns
        ;; ligatures   ; ligatures and symbols to make your code pretty again
        ;;minimap           ; show a map of the code on the side
        modeline          ; snazzy, Atom-inspired modeline, plus API
@@ -69,7 +69,7 @@
        ;; lispy             ; vim for lisp, for people who don't like vim
        ;; multiple-cursors  ; editing in many places at once
        ;;objed             ; text object editing for the innocent
-       parinfer          ; turn lisp into python, sort of
+       ;; parinfer          ; turn lisp into python, sort of
        rotate-text       ; cycle region at point between text candidates
        snippets          ; my elves. They type so I don't have to
        ;;word-wrap         ; soft wrapping with language-aware indent
