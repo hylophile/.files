8dec6579411f7b8d0936eb8e0ee1789eb2c34124
hylo <hylo@posteo.de>
2024-03-23 20:09:30 +0100

    fix: re-add vertical border patch

--- .config/nixos/shared/emacs/emacs.nix
+++ .config/nixos/shared/emacs/emacs.nix
@@ -10,7 +10,7 @@
     (pkgs.emacs-pgtk.overrideAttrs {
       patches = [
         ./support-image-transparent.patch
-        # ./vertical-border-respect-alpha-background.patch
+        ./vertical-border-respect-alpha-background.patch
         ./fringe-respect-alpha-background.patch
         ./add-rgba-support-for-faces-to-pgtk.patch
       ];
