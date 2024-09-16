aa21dac0c94887359f1ed35fa679b86dbc25aa7d
hylo <hylo@posteo.de>
2024-09-16 09:50:14 +0200

    fix: hass script

--- scripts/hass.sh
+++ scripts/hass.sh
@@ -14,7 +14,7 @@ if [ "$1" = "monitors" ]; then
     curl --silent -X POST \
         -H "Authorization: Bearer $token" \
         -H "Content-Type: application/json" \
-        -d '{"entity_id": "light.monitor_kl"}' \
+        -d '{"entity_id": "light.monitor_big2"}' \
         "http://192.168.0.52:8123/api/services/light/toggle" >/dev/null &
     curl --silent -X POST \
         -H "Authorization: Bearer $token" \
