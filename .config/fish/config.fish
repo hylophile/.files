d4f6d6d2c14461a83bff1d8bd30609dd934ad14e
hylo <hylo@posteo.de>
2024-09-16 09:48:23 +0200

    fix: dolphin apps

--- .config/fish/config.fish
+++ .config/fish/config.fish
@@ -11,8 +11,10 @@ distrobox_auto_setup
 set -gx XDG_CURRENT_DESKTOP sway
 set -gx MOZ_ENABLE_WAYLAND 1
 set -gx _JAVA_AWT_WM_NONREPARENTING 1
-
 set -gx EDITOR hx
+set -gx XDG_MENU_PREFIX arch-
+
+# abbr --add hx helix
 
 fish_add_path ~/.nix-profile/bin
 fish_add_path ~/.local/bin
