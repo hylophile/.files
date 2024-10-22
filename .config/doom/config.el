39307ed973597220a3f3546478cf685e4055bd99
hylo <hylo@posteo.de>
2024-10-22 20:41:59 +0200

    feat: doom font

--- .config/doom/config.el
+++ .config/doom/config.el
@@ -21,7 +21,8 @@
 ;; See 'C-h v doom-font' for documentation and more examples of what they
 ;; accept. For example:
 ;;
-(setq doom-font (font-spec :family "Agave" :size 18 :weight 'semi-light)
+(setq doom-font (font-spec :family "hylosevka" :size 18
+                           :weight 'semi-light)
       doom-variable-pitch-font (font-spec :family "Jost" :size 13))
 ;;
 ;; If you or Emacs can't find your font, use 'M-x describe-font' to look them
