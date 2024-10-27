04d164847fdc3911993457b6764435884ff654c7
hylo <hylo@posteo.de>
2024-10-27 15:48:50 +0100

    feat: minimal doom config

--- .config/doom/config.el
+++ .config/doom/config.el
@@ -1,77 +1,66 @@
 ;;; $DOOMDIR/config.el -*- lexical-binding: t; -*-
 
-;; Place your private configuration here! Remember, you do not need to run 'doom
-;; sync' after modifying this file!
-
-
-;; Some functionality uses this to identify you, e.g. GPG configuration, email
-;; clients, file templates and snippets. It is optional.
-;; (setq user-full-name "John Doe"
-;;       user-mail-address "john@doe.com")
-
-;; DOOM exposes five (optional) variables for controlling fonts in Doom:
-;;
-;; - `doom-font' -- the primary font to use
-;; - `doom-variable-pitch-font' -- a non-monospace font (where applicable)
-;; - `doom-big-font' -- used for `doom-big-font-mode'; use this for
-;;   presentations or streaming.
-;; - `doom-symbol-font' -- for symbols
-;; - `doom-serif-font' -- for the `fixed-pitch-serif' face
-;;
-;; See 'C-h v doom-font' for documentation and more examples of what they
-;; accept. For example:
-;;
 (setq doom-font (font-spec :family "hylosevka" :size 18
                            :weight 'semi-light)
       doom-variable-pitch-font (font-spec :family "Jost" :size 13))
-;;
-;; If you or Emacs can't find your font, use 'M-x describe-font' to look them
-;; up, `M-x eval-region' to execute elisp code, and 'M-x doom/reload-font' to
-;; refresh your font settings. If Emacs still can't find your font, it likely
-;; wasn't installed correctly. Font issues are rarely Doom issues!
+
 (setq confirm-kill-emacs nil)
-;; There are two ways to load a theme. Both assume the theme is installed and
-;; available. You can either set `doom-theme' or manually load a theme with the
-;; `load-theme' function. This is the default:
 (setq doom-theme 'doom-challenger-deep)
-
-;; This determines the style of line numbers in effect. If set to `nil', line
-;; numbers are disabled. For relative line numbers, set this to `relative'.
 (setq display-line-numbers-type t)
-
-;; If you use `org' and don't want your org files in the default location below,
-;; change `org-directory'. It must be set before org loads!
 (setq org-directory "~/org/")
 
 
-;; Whenever you reconfigure a package, make sure to wrap your config in an
-;; `after!' block, otherwise Doom's defaults may override your settings. E.g.
-;;
-;;   (after! PACKAGE
-;;     (setq x y))
-;;
-;; The exceptions to this rule:
-;;
-;;   - Setting file/directory variables (like `org-directory')
-;;   - Setting variables which explicitly tell you to set them before their
-;;     package is loaded (see 'C-h v VARIABLE' to look up their documentation).
-;;   - Setting doom variables (which start with 'doom-' or '+').
-;;
-;; Here are some additional functions/macros that will help you configure Doom.
-;;
-;; - `load!' for loading external *.el files relative to this one
-;; - `use-package!' for configuring packages
-;; - `after!' for running code after a package has loaded
-;; - `add-load-path!' for adding directories to the `load-path', relative to
-;;   this file. Emacs searches the `load-path' when you load packages with
-;;   `require' or `use-package'.
-;; - `map!' for binding new keys
-;;
-;; To get information about any of these functions/macros, move the cursor over
-;; the highlighted symbol at press 'K' (non-evil users must press 'C-c c k').
-;; This will open documentation for it, including demos of how they are used.
-;; Alternatively, use `C-h o' to look up a symbol (functions, variables, faces,
-;; etc).
-;;
-;; You can also try 'gd' (or 'C-c c d') to jump to their definition and see how
-;; they are implemented.
+(defun org-archive-done-tasks ()
+  (interactive)
+  (mapc (lambda(entry)
+          (goto-char entry)
+          (org-archive-subtree))
+        (reverse (org-map-entries (lambda () (point)) "TODO=\"DONE\"" 'file))))
+
+;; A bit of setup for managing a bare dotfile repository. We check whether the
+;; current =default-directory= belongs to our dotfiles with =git ls-files=. If
+;; it does, we add our dotfile environment. We also memoize previous calls to
+;; =git=, since magit calls =magit-process-environment= quite often (~25 times
+;; per =magit-status=), which slows down every =magit=-command with our advice.
+;; inspired by https://github.com/magit/magit/issues/460#issuecomment-1475082958
+(setq hy/dotfile-dirs ())
+
+(defun hy/magit-process-environment (env)
+  "Detect and set git -bare repo env vars when in tracked dotfile directories."
+  (let* ((git-dir (expand-file-name "~/.dotfiles/"))
+         (work-tree (expand-file-name "~/"))
+         (default (file-name-as-directory (expand-file-name default-directory))))
+    (unless (assoc default hy/dotfile-dirs)
+      (push (cons default (eq 0 (call-process "/usr/bin/env" nil nil nil
+                                              "git"
+                                              (format "--git-dir=%s" git-dir)
+                                              (format "--work-tree=%s" work-tree)
+                                              "ls-files"
+                                              "--error-unmatch"
+                                              default)))
+            hy/dotfile-dirs))
+    (when (cdr-safe (assoc default hy/dotfile-dirs))
+      (push (format "GIT_WORK_TREE=%s" work-tree) env)
+      (push (format "GIT_DIR=%s" git-dir) env)))
+  env)
+
+(advice-add 'magit-process-environment
+            :filter-return #'hy/magit-process-environment)
+
+
+;; When we want to add a new untracked dotfile in a new directory, we have no
+;; way of knowing that it's a dotfile with the above advice. As a workaround, we
+;; just stage the file in our dotfiles if there's no =magit-gitdir=, and
+;; otherwise fallback to =magit-stage-file= (which otherwise works as expected
+;; in known dotfile directories, even for untracked files).
+
+(defun hy/magit-stage-file ()
+  (interactive)
+  (if (magit-gitdir)
+      (call-interactively #'magit-stage-file)
+    (shell-command (concat
+                    "git --git-dir=$HOME/.dotfiles/ --work-tree=$HOME add "
+                    (buffer-file-name))
+                   t)))
+
+(map! :after magit :leader "g S" #'hy/magit-stage-file)
