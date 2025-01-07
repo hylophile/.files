8795ddc7afaf66ccb8c5e6c045fb009e5ee8127e
hylo <hylo@posteo.de>
2025-01-07 13:55:06 +0100

    feat: stuff

--- .config/fish/functions/remove-packages.fish
+++ .config/fish/functions/remove-packages.fish
@@ -1,5 +1,5 @@
 function remove-packages
-    sudo pacman -R (pacgraph -c | rg -v "^warning" | head -n 100 | fzf -m | cut -d' ' -f 2)
+    sudo pacman -R (pacgraph -c | rg -v "^warning" | head -n 100 | fzf -m | cut -d' ' -f 3)
     sudo pacman -Qtdq | sudo pacman -Rns -
     sudo pacman -Qqd | sudo pacman -Rsu -
 end
