1ecbc14ff6b18328def74e25cdff253eb54be04f
hylo <hylo@posteo.de>
2025-03-02 20:44:20 +0100

    fix: use environment.d for truly global envvars (fixes spawning hx from wezterm)

--- .config/fish/config.fish
+++ .config/fish/config.fish
@@ -8,22 +8,6 @@ end
 
 distrobox_auto_setup
 
-set -gx XDG_CURRENT_DESKTOP sway
-set -gx MOZ_ENABLE_WAYLAND 1
-set -gx _JAVA_AWT_WM_NONREPARENTING 1
-
-# set -gx EDITOR /usr/bin/kak
-set -gx EDITOR /usr/bin/hx
-
-set -gx XDG_MENU_PREFIX arch-
-
-# abbr --add hx helix
-
-fish_add_path ~/.nix-profile/bin
-fish_add_path ~/.local/bin
-fish_add_path ~/.cargo/bin
-fish_add_path ~/.config/emacs/bin
-
 
 function ls --wraps=lsd --description 'alias ls=lsd'
     lsd $argv
