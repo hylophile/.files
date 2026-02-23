94d4f283fcfcefce3010653b1ecec01794b1a6ee
hylo <hylo@posteo.de>
2026-02-23 23:48:08 +0100

    feat:new lights

--- scripts/hass.sh
+++ scripts/hass.sh
@@ -22,3 +22,19 @@ if [ "$1" = "monitors" ]; then
         -d '{"entity_id": "light.huewa1"}' \
         "http://192.168.0.52:8123/api/services/light/toggle" >/dev/null &
 fi
+
+if [ "$1" = "left" ]; then
+    curl --silent -X POST \
+        -H "Authorization: Bearer $token" \
+        -H "Content-Type: application/json" \
+        -d '{"entity_id": "light.huewa1"}' \
+        "http://192.168.0.52:8123/api/services/light/toggle" >/dev/null &
+fi
+
+if [ "$1" = "right" ]; then
+    curl --silent -X POST \
+        -H "Authorization: Bearer $token" \
+        -H "Content-Type: application/json" \
+        -d '{"entity_id": "light.huewa2"}' \
+        "http://192.168.0.52:8123/api/services/light/toggle" >/dev/null &
+fi
